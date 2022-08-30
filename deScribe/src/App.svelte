c44b19e19d0211eb5225a50bd0b797935bdfeb7c
Antonio Leitao <aml.leitao@campus.fct.unl.pt>
2022-08-30 15:35:38 +0200

    added spa routing to aplication

--- deScribe/src/App.svelte
+++ deScribe/src/App.svelte
@@ -1,45 +1,21 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+	import Router, {location, link} from 'svelte-spa-router';
+	import Home from './lib/themes/basic/Home.svelte';
+	import About from './lib/themes/basic/About.svelte';
+	import Blog from './lib/themes/basic/Blog.svelte';
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<nav>
+	<a  href="/">Home</a>
+	<a href="/#/about">About</a>
+	<a href="/#/blog/colors">Colors</a>
+</nav>
 
-  <div class="card">
-    <Counter />
-  </div>
+<Router routes={{
+	'/': Home,
+	'/about': About,
+	'/blog/:blogName': Blog
+}} />
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+<hr>
+/#{$location}
